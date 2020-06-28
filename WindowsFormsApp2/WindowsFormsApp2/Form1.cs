268e11cddd01bf0b82b9b383183329c8c940f44d
Marc Raymond <59947352+mkr123456@users.noreply.github.com>
2020-06-27 20:52:04 -0700

    music player
    project is saves as windowsFormsApp2 , but its the music player

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                textBox1.Text = openFileDialog1.FileName;
+
+            }
+            Player.URL = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.play();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.pause();
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.stop();
+        }
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.play();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.stop();
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+
